0cb3352808dbf0908d314455d528ab6607c53ee4
Number5IsAlive <james.s.mason@hotmail.com>
2022-02-04 10:46:45 +0000

    2.2 Opening an editor window

--- Assets/Scripts/Story/Editor/StoryEditor.cs
+++ Assets/Scripts/Story/Editor/StoryEditor.cs
@@ -1,9 +1,27 @@
 using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace Adventure.Story.Editor
 {
     public class StoryEditor : EditorWindow
     {
-        
+        [MenuItem("Window/Text Adventure/Story Editor")]
+        public static void ShowEditorWindow()
+        {
+            GetWindow(typeof(StoryEditor), false, "Story Editor", true);
+        }
+
+        [OnOpenAsset(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            StorySO storySO = EditorUtility.InstanceIDToObject(instanceID) as StorySO;
+            if (storySO != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }
     }
 }
